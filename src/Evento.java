d4946fd394e19a46ac94ae60b23ee4fba5c1babd
MrFranks89 <federico.franchi89@gmail.com>
2024-10-20 12:29:30 +0200

    ADD: getter, setter, exceptions

--- src/Evento.java
+++ src/Evento.java
@@ -8,42 +8,68 @@ public class Evento {
 
 	private String titoloEvento;
 	private LocalDate dataEvento; 
-	private int postiTotali;
+	private final int postiTotali;
 	private int postiPrenotati = 0;
 	
-	
-	public static void main(String[] args) {
-	
-	 try {
-		 sceltaDataUtente();
-   } catch(IllegalArgumentException e) {
-	   System.err.println("è stato inserita una data sbagliata.");
-   } catch(InputMismatchException e) {
-	   System.err.println("Input errato: " + e);
-   }
-	   System.out.println("Scelta utente finita.");
+	public Evento(String titoloEvento, LocalDate dataEvento, int postiTotali) throws IllegalArgumentException {
+		if (dataEvento.isBefore(LocalDate.now())) {
+			throw new IllegalArgumentException("La data inserita è già passata. Inserirne una nuova");
+		}
+		
+		if (postiTotali <= 0) {
+			throw new IllegalArgumentException("Il numero di posti totali deve essere positivo");
+		}
+		
+		this.titoloEvento = titoloEvento;
+		this.dataEvento = dataEvento;
+		this.postiTotali = postiTotali;
+		this.postiPrenotati = 0;
 	}
-	 
-	private static void sceltaDataUtente() {
-		Scanner scan = new Scanner (System.in);
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        
-        LocalDate dataInserita = null;
-        boolean dataValida = false;
-        
-        while (!dataValida) {
-            System.out.println("Inserisci una data nel formato dd/MM/yyyy: ");
-            String input = scan.nextLine();
-            
-        try {
-            dataInserita = LocalDate.parse(input, formatter);
-            dataValida = true;
-        } catch (DateTimeParseException e) {
-                System.out.println("Formato data non valido. Riprova.");
-        }	
+	
+	
+	public String getTitoloEvento() {
+		return titoloEvento;
 	}
-  }
+
+	public void setTitoloEvento(String titoloEvento) {
+		this.titoloEvento = titoloEvento;
+	}
+
+	public LocalDate getDataEvento() {
+		return dataEvento;
+	}
+
+	public void setDataEvento(LocalDate dataEvento) {
+		this.dataEvento = dataEvento;
+	}
+	
+	public void setData(LocalDate data) throws IllegalArgumentException {
+        if (data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data inserita è già passata.");
+        }
+        this.dataEvento = data;
+    }
+
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
+	
+	public void prenotaPosti(int numeroPosti) throws IllegalArgumentException{
+		if (numeroPosti <= 0) {
+			throw new IllegalArgumentException("Il numero deve essere positivo ");
+		}
+		if (postiPrenotati + numeroPosti > postiTotali) {
+			throw new IllegalArgumentException("I posti sono esauriti");
+		}
+		
+		postiPrenotati += numeroPosti;
+	}
+	
+	public int postiDisponibili() {
+        return postiTotali - postiPrenotati;
+    }
 }
-
-	
-
